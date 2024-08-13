59f788fa3c20e3d2582f9bedd173480742e54447
RYC0208 <ryc0208@naver.com>
2024-08-13 17:37:07 +0900

    inputFields 객체 데이터 속성 통일

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -4,12 +4,7 @@ import Button from "./Button";
 
 const Form = ({ formData, updateFormData, addCountry, updateCountry }) => {
   const inputFields = [
-    {
-      label: "국가명",
-      name: "country",
-      type: "text",
-      placeholder: "국가 입력",
-    },
+    {label: "국가명",name: "country",type: "text",placeholder: "국가 입력", },
     { label: "금메달", name: "gold", type: "number", placeholder: "0" },
     { label: "은메달", name: "silver", type: "number", placeholder: "0" },
     { label: "동메달", name: "bronze", type: "number", placeholder: "0" },
