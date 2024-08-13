d20d23c75419031385352ee68b91871cc634143b
RYC0208 <ryc0208@naver.com>
2024-08-13 18:29:17 +0900

    메달에 수에 따라서 정렬 기능추가

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,44 +1,49 @@
 import React from "react";
 
 const List = ({ countries, setCountries }) => {
+  const sortedCountries = [...countries].sort((a, b)=> {
+    if(b.gold !== a.gold) return b.gold - a.gold;
+    if(b.silver !== a.silver) return b.silver - a.silver;
+    return b.bronze - a.bronze;
+  })
   return (
     <div>
-    {countries.length === 0 ? (
-      <p>등록된 국가가 없습니다.</p>
-    ) : (
-      <table>
-        <thead>
-          <tr>
-            <th>국가명</th>
-            <th>금메달</th>
-            <th>은메달</th>
-            <th>동메달</th>
-            <th>액션</th>
-          </tr>
-        </thead>
-        <tbody>
-          {countries.map((country, index) => (
-            <tr key={index} style={{ backgroundColor: index % 2 === 0 ? '#e0e0e0f8' : '#f7f7f7f8' }}>
-              <td>{country.country}</td>
-              <td>{country.gold}</td>
-              <td>{country.silver}</td>
-              <td>{country.bronze}</td>
-              <td>
-                <button
-                  onClick={() => {
-                    const filteredCountry = countries.filter((c) => c !== country);
-                    setCountries(filteredCountry);
-                  }}
-                >
-                  삭제
-                </button>
-              </td>
+      {sortedCountries.length === 0 ? (
+        <p>등록된 국가가 없습니다.</p>
+      ) : (
+        <table>
+          <thead>
+            <tr>
+              <th>국가명</th>
+              <th>금메달</th>
+              <th>은메달</th>
+              <th>동메달</th>
+              <th>액션</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    )}
-  </div>
+          </thead>
+          <tbody>
+            {sortedCountries.map((country, index) => (
+              <tr key={index} style={{ backgroundColor: index % 2 === 0 ? '#e0e0e0f8' : '#f7f7f7f8' }}>
+                <td>{country.country}</td>
+                <td>{country.gold}</td>
+                <td>{country.silver}</td>
+                <td>{country.bronze}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      const filteredCountry = countries.filter((c) => c !== country);
+                      setCountries(filteredCountry);
+                    }}
+                  >
+                    삭제
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
   );
 };
 
